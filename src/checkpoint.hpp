47f79b427fac7edee942b14d94266a26c8e79c08
Kaushik Ghose <kaushik.ghose@gmail.com>
2017-10-31 13:18:55 -0400

    tests: full test suite for Checkpoint
    bugfixes to Checkpoint code

--- src/checkpoint.hpp
+++ src/checkpoint.hpp
@@ -3,14 +3,53 @@
 */
 #pragma once
 
+#include <memory>
 #include <vector>
-#include "spaceshipstate.hpp"
-#include "event.hpp"
 
-union Checkpoint
+namespace sim
 {
-  SpaceShipState state;
-  Event          event;
+
+struct Checkpoint
+{
+  double julian_date;
+  Checkpoint( double jd ) { julian_date = jd; }
 };
 
-typedef std::vector<Checkpoint> checkpointvec_t;
+typedef std::shared_ptr<Checkpoint> checkpointp_t;
+typedef std::vector<checkpointp_t> checkpointvec_t;
+
+// return first index that is on or after given date
+// return checkpoints.size() if past end of data
+inline size_t 
+find( double jd, const checkpointvec_t& checkpoints )
+{
+  if( checkpoints.size() == 0 )              return 0;
+  if( checkpoints.back()->julian_date < jd ) return checkpoints.size();  
+  if( checkpoints[ 0 ]->julian_date >= jd )  return 0;
+  
+  size_t i0 = 0
+       , i2 = checkpoints.size() - 1
+       , i1;
+  for(;;)
+  {
+    if( checkpoints[ i0 ]->julian_date == jd ) return i0;
+    if( checkpoints[ i2 ]->julian_date == jd ) return i2;
+    if( i2 == i0 + 1 )                         return i2;
+
+    i1 = ( i2 + i0 ) / 2;
+    if( checkpoints[ i1 ]->julian_date == jd ) return i1;
+    if( checkpoints[ i1 ]->julian_date < jd )  { i0 = i1; }
+    else                                       { i2 = i1; }
+  }
+}
+
+// discard checkpoints on or after given date
+inline void 
+discard_stale_data( double jd, checkpointvec_t& checkpoints )
+{
+  size_t i = find( jd, checkpoints );
+  if( i == checkpoints.size() ) return;
+  checkpoints.resize( i );
+}
+
+} // namespace sim
--- tests/checkpoints.cpp
+++ tests/checkpoints.cpp
@@ -8,7 +8,6 @@ using namespace sim;
 
 SCENARIO( "Checkpoints are a mixture of SpaceshipState and Events", "[checkpoints]" ) 
 {
-  
   GIVEN( "A an empty checkpoint vector" ) 
   {
     checkpointvec_t v;
@@ -23,8 +22,106 @@ SCENARIO( "Checkpoints are a mixture of SpaceshipState and Events", "[checkpoint
       
       THEN( "We can retrieve them" ) 
       {
-          REQUIRE( v[0]->julian_date == 100.0 );
-          REQUIRE( v[2]->julian_date == 120.0 );
+        REQUIRE( v[0]->julian_date == 100.0 );
+        REQUIRE( v[2]->julian_date == 120.0 );
+      }      
+    }
+  }
+}
+
+
+SCENARIO( "We can find data by time in checkpoints", "[checkpoints-find]" ) 
+{
+  GIVEN( "An empty checkpoint vector" ) 
+  {
+    checkpointvec_t v;
+    WHEN( "We look for data" ) 
+    {
+      double jd = 100;
+      size_t idx = find( jd, v );
+      THEN( "Our index should always be 0" ) 
+      {
+        REQUIRE( idx == 0 );
+      }
+    }
+  }
+
+  GIVEN( "A checkpoint vector with a mixture of Events and SpaceShipState" ) 
+  {
+    int jd_min = 5, jd_max = 99;
+
+    checkpointvec_t v;
+    for( int i = jd_min; i < jd_max; i+=10 )
+    {
+      v.push_back( checkpointp_t( new Event( i ) ) );   
+      v.push_back( checkpointp_t( new SpaceShipState( i + 5 ) ) );   
+    }
+    
+    WHEN( "We look for data after the last checkpoint" ) 
+    {
+      double jd = 110;
+      size_t idx = find( jd, v );
+
+      THEN( "Our index should be size of array" ) 
+      {
+        REQUIRE( idx == v.size() );
+      }
+    }
+    
+    WHEN( "We look for data before the first checkpoint" ) 
+    {
+      double jd = 0;
+      size_t idx = find( jd, v );
+
+      THEN( "Our index should be 0" ) 
+      {
+        REQUIRE( idx == 0 );
+      }
+    }
+    
+    WHEN( "We look for events after a certain date within our data" ) 
+    {
+      THEN( "We should get the first checkpoint on or after that date" ) 
+      {
+        for( double jd = jd_min; jd < jd_max; jd++ )
+        {
+          size_t idx = find( jd, v );
+          if( idx == v.size() ) 
+          {
+            REQUIRE( jd > v.back()->julian_date );            
+          }
+          else
+          {
+            if( idx > 0 ) { REQUIRE( v[ idx - 1 ]->julian_date < jd ); }
+            REQUIRE( v[ idx ]->julian_date >= jd );
+            if( idx < v.size() - 1 ) { REQUIRE( v[ idx + 1 ]->julian_date > jd ); }            
+          }
+        }
+      }
+    }
+  }
+}
+
+
+SCENARIO( "We can discard checkpoints after a given time", "[checkpoints-discard]" ) 
+{
+  GIVEN( "A checkpoint vector with a mixture of Events and SpaceShipState" ) 
+  {
+    int jd_min = 5, jd_max = 99;
+    checkpointvec_t v;
+    for( int i = jd_min; i < jd_max; i+=10 )
+    {
+      v.push_back( checkpointp_t( new Event( i ) ) );   
+      v.push_back( checkpointp_t( new SpaceShipState( i + 5 ) ) );   
+    }
+    
+    WHEN( "We ask to discard data after a given time point" ) 
+    {
+      double jd = 53;
+      discard_stale_data( jd, v );
+      THEN( "Our last data point should end before that time" ) 
+      {
+        REQUIRE( v.back()->julian_date <= jd );
       }
     }
   }
