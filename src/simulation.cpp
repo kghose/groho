375bc12fb7c64be310b4461ba277d2809b380200
Kaushik Ghose <kaushik.ghose@gmail.com>
2017-11-01 22:48:29 -0400

    Adding in orrery and space fleet
    Also refactored user command boolean and mutex names

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -5,15 +5,20 @@
 #include "loguru.hpp"
 
 #include "simulation.hpp"
+#include "event.hpp"
+
+#include "simplesolarsystem.hpp"  // just for testing
 
 namespace sim
 {
 
 Simulation::Simulation( std::string scenario_file ) 
 {
-  action = false;
+  user_command_received = false;
   quit_now = false;
   restart = false;
+
+  orrery.load( load_simple_solar_system( scenario_file ) );
 }
 
 void 
@@ -21,7 +26,7 @@ Simulation::loop()
 {
   while( !quit_now )
   {
-    wait_for_action();
+    wait_for_user_command();
     if( quit_now ) return;
     if( restart ) 
     { 
@@ -32,18 +37,18 @@ Simulation::loop()
 }
 
 void
-Simulation::wait_for_action()
+Simulation::wait_for_user_command()
 {
-  std::unique_lock<std::mutex> lk(m);
-  cv.wait(lk, [this](){return action;});  // protects against spurious unblocks
-  action = false;
+  std::unique_lock<std::mutex> lk( user_command_mutex );
+  cv.wait(lk, [this](){ return user_command_received; });  // protects against spurious unblocks
+  user_command_received = false;
 }
 
 void
 Simulation::quit()
 { 
-  std::lock_guard<std::mutex> lk(m);
-  action = true;
+  std::lock_guard<std::mutex> lk( user_command_mutex );
+  user_command_received = true;
   quit_now = true;
   cv.notify_one();
 }
@@ -51,9 +56,9 @@ Simulation::quit()
 void 
 Simulation::run( SimulationParameters sp )
 {
-  std::lock_guard<std::mutex> lk(m);
+  std::lock_guard<std::mutex> lk( user_command_mutex );
   simulation_parameters = sp;
-  action = true;  
+  user_command_received = true;  
   restart = true;
   cv.notify_one();
 }
@@ -67,11 +72,45 @@ Simulation::simulation_loop( SimulationParameters sp )
   double jd = sp.jd_start;
   while( !restart & !quit_now & ( jd < sp.jd_end ) ) 
   { 
-    //do simulation
-    jd += 1;
-    std::cout << jd << std::endl;
-    sleep(1);
+    step( jd, sp.dt);
+    jd += sp.dt;
+    //std::cout << jd << std::endl;
+  }
+
+  DLOG_S(INFO) << "Stopping simulation: " << jd;    
+}
+ 
+void
+Simulation::step( double jd, double dt )
+{
+  orrery.propagate( jd );
+  propagate_space_fleet( jd, dt );
+  resolve_actions( jd );
+}
+
+void 
+Simulation::propagate_space_fleet( double jd, double dt )
+{
+  for( auto& [k, v] : space_fleet )
+  {
+    bool bingo_fuel = false;
+    v.update_state( dt, orrery.compute_g( v.get_pos() ), bingo_fuel);
+    if( bingo_fuel )
+    {
+      checkpoints.push_back( checkpointp_t( new Event( 
+        jd,
+        EventType::BingoFuel,
+        v.name,
+        ""
+      )));
+    }
   }
 }
-  
+
+void
+Simulation::resolve_actions( double jd )
+{
+
+}
+
 }
--- src/simulation.hpp
+++ src/simulation.hpp
@@ -9,6 +9,9 @@
 #include <condition_variable>
 
 #include "checkpoint.hpp"
+#include "orrery.hpp"
+#include "spaceship.hpp"
+
 
 namespace sim
 {
@@ -34,31 +37,49 @@ class Simulation
                                   // and rerun with new time range supplied
                                 ;
 
-  bool                    action  // some action is required. Used to unblock `loop`
+  bool     user_command_received  // some action is required. Used to unblock `loop`
                                 ;
-  std::mutex m;
+  std::mutex user_command_mutex;
   std::condition_variable cv;
 
   SimulationParameters  simulation_parameters;
   Checkpoints checkpoints;
 
+  Orrery orrery;
+  std::unordered_map<std::string, SpaceShip> space_fleet;
+
 public:
   Simulation( std::string scenario_file );
 
-  // Block thread until we have something to do
-  void wait_for_action();
-
   void quit();
   
+  void loop();
   // wait until we are told to simulate then keep simulating until done
   // or told to resimulate. If done, return to waiting
-  void loop();
 
-  // Execute simulation with given parameters
   void run( SimulationParameters sp );
-    
-  // Run simulation until all time steps are done, or we are asked to quit
+  // Execute simulation with given parameters
+
+  const Orrery& get_orrery() { return orrery; }
+  // Meant for display routines who would like to display the solar system
+
+private:
+
+  void wait_for_user_command();  
+  // Block thread until we have something to do
+
   void simulation_loop( SimulationParameters sp );
+  // Run simulation until all time steps are done, or we are asked to quit
+
+  void step( double jd, double dt );
+  // Run simulation for one time-step and collect checkpoints as needed
+
+  void propagate_space_fleet( double jd, double dt );
+  // Update spaceship positions, collect checkpoints as needed
+
+  void resolve_actions( double jd );
+  // If there are any actions or interactions, resolve them.
+  // Add checkpoints and change spacehsip states as needed
 };
 
 }
