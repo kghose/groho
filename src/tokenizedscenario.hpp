4aff7bc60f400c8d3bfe470e8436824c13118107
Kaushik Ghose <kaushik.ghose@gmail.com>
2017-11-09 13:36:57 -0500

    Token partially handles reading of scenario files from disk
    It converts the text in the file into a series of tokens that we can
    then parse further. Discards comments, trims strings and flags parsing
    errors
    
    Tests are included

---
+++ src/tokenizedscenario.hpp
@@ -0,0 +1,109 @@
+/*
+  Code to handle the common aspects of parsing scenario and flightplan files
+
+  Every entry in an input file is of the pattern
+
+  text: text
+  number: text
+  
+  Comments: If a # is found anywhere the rest of that line is discarded
+
+  Header: We expect all files to begin with the "text: text" pattern 
+
+  Program: flight plan files have a second section that is all "number: text"
+
+*/
+#pragma once
+
+#include <string>
+#include <vector>
+#include <istream>
+#include <regex>
+
+#define LOGURU_WITH_STREAMS 1
+#include "loguru.hpp"
+
+
+namespace sim
+{
+
+struct Token
+{
+  std::string key, value;
+};
+
+struct TokenizedScenario
+{
+  std::vector<Token> header, program;
+  // main scenario files only have headers
+  // flight plans have both a header and a program
+  bool contains_errors;
+  size_t line_with_error;
+  std::string error_message;
+  // We try not to crash on malformed inputs - that would be annoying
+  // Instead we flag the scenario as corrupt and return an informative message
+
+  Token get_header_value( std::string key )
+  {
+    for( auto& tk : header ) { if( tk.key == key ) return tk; }
+    return Token();  // empty key is sentinel for not found
+  }
+};
+
+
+std::string trim( const std::string& in )
+{
+  size_t i0 = in.find_first_not_of(' '),
+         i1 = in.find_last_not_of(' ');
+  return in.substr(i0, i1 - i0 + 1);
+}
+
+
+std::regex number_pattern("-?[0-9]+.?[0-9]+");
+
+std::istream& operator >> ( std::istream& scn_file, TokenizedScenario& ts )
+{
+  size_t idx, line_ctr = 0;
+  std::string line;
+
+  while( std::getline( scn_file, line ) )  
+  {
+    line_ctr++;
+    // Chomp comment
+    idx = line.find_first_of("#");
+    if( idx != -1 ) { line = line.substr(0, idx); }
+    // Ignore functionally empty lines
+    if( line.length() == 0 ) continue;
+
+    idx = line.find_first_of(":");
+    if( idx == -1 ) continue;
+    
+    Token t;
+    t.key = trim( line.substr(0, idx) );
+    t.value = trim( line.substr(idx + 1, -1) );
+    
+    if( (t.key.length() == 0) | (t.value.length() == 0) )
+    {
+      ts.contains_errors = true;
+      ts.line_with_error = line_ctr;
+      ts.error_message = "Key or value missing on line " + std::to_string( line_ctr );
+      break;
+    }
+
+    bool key_is_number = std::regex_match( t.key, number_pattern);
+    if( key_is_number ) { ts.program.push_back( t ); }
+                   else {  ts.header.push_back( t ); }
+
+    //DLOG_S(INFO) << "(" << t.key << "), (" << t.value << ")";
+  }
+
+  return scn_file;
+}
+
+
+
+
+
+} // namespace sim
+
+
