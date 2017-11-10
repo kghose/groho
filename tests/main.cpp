e53bd5ba1cd91abacde1bc0b5f76ada11b9acd93
Kaushik Ghose <kaushik.ghose@gmail.com>
2017-11-09 20:41:08 -0500

    tests: cleaning up

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-#define LOGURU_IMPLEMENTATION 1
+#define LOGURU_IMPLEMENTATION 1  // Otherwise compile error
 #include "loguru.hpp"
