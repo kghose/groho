d1037968920fc1ab1ed70f33209cfb0c47a1646a
Kaushik Ghose <kaushik.ghose@gmail.com>
2017-12-13 19:24:31 -0500

    feature: pretty printing of event

--- src/event.hpp
+++ src/event.hpp
@@ -4,18 +4,20 @@
 #pragma once
 
 #include <string>
-//#include "checkpoint.hpp"
+#include <iostream>
+
 
 namespace sim
 {
 
 enum class EventType { 
-  Launch, 
+  Launch = 0, 
   Landing,
   Crash,
   Encounter,
   BingoFuel
 };
+
 
 struct Event
 {
@@ -26,11 +28,32 @@ struct Event
 
   Event() {}
   Event( double jd ): jd( jd ) {}
-  Event( double jd, EventType et ) : jd( jd ), event_type( et ) {}
-  Event( double jd, EventType et, std::string name, std::string remarks ) : 
-      jd( jd ), name( name ), remarks( remarks ), event_type( et ) 
-  {}  
+  Event( double jd, std::string name, EventType et ) : 
+      jd( jd ), name( name ), event_type( et ) {}
+  Event( double jd, std::string name, EventType et, std::string remarks ) : 
+      jd( jd ), name( name ), remarks( remarks ), event_type( et ) {}
+
+  std::string event_type_str() const
+  {
+    switch( event_type ) {
+      case EventType::Launch: return "Launch";
+      case EventType::Landing: return "Landing";
+      case EventType::Crash: return "Crash";
+      case EventType::Encounter: return "Encounter";
+      case EventType::BingoFuel: return "BingoFuel";
+      default: return "Unknown";
+    }
+  }
 };
+
+inline
+std::ostream& 
+operator << ( std::ostream& os, const Event& e )
+{
+  os << e.jd << ", " << e.name << ", " << e.event_type_str() << ", " << e.remarks;
+  return os;
+}
+
 
 inline bool operator == ( const Event& e, const float _jd ) { return e.jd == _jd; }
 inline bool operator == ( const float _jd, const Event& e ) { return e.jd == _jd; }
