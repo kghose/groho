01d18cb85778738b7b42e567e2a7b0f6932afbef
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-04-27 21:41:00 -0400

    Can now read SPK upto element summaries
    1. Need to add code to read coefficents
    2. Improve docs

--- src/orrery/spk.cpp
+++ src/orrery/spk.cpp
@@ -1,22 +1,28 @@
 /*
 
   Segments are organized into a list (vector) such that any object that serves
-  as a center (reference) for a body appears earlier 
-  a 'center' appears earlier in the list than any object tha 
+  as a center (reference) for a body appears earlier
+  a 'center' appears earlier in the list than any object tha
   (such as barycenters) are placed before any bodies that are computed
-  with reference to them. 
-  
-  
-  For this reason body 3 (Earth-Moon barycenter) 
+  with reference to them.
+
+
+  For this reason body 3 (Earth-Moon barycenter)
   referenced to the solar system barycenter 0, is placed before 399 (Earth)
   or 301 (moon). This sorting is done by placing bodies <= 10 at the begining
-  of the list. The order of succeeding bodies is irrelevant 
+  of the list. The order of succeeding bodies is irrelevant
 
   Thanks go to JPL/NASA NAIF docs
   ftp://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/req/daf.html
 
   And jplemphem Python code by Brandon Rhodes
   https://github.com/brandon-rhodes/python-jplephem
+
+  And this stackoverflow answer
+  https://space.stackexchange.com/a/20715
+
+  to this question
+  https://space.stackexchange.com/questions/12506/what-is-the-exact-format-of-the-jpl-ephemeris-files
 
 */
 #include <fstream>
@@ -26,34 +32,38 @@
 #define LOGURU_WITH_STREAMS 1
 #include "loguru.hpp"
 
-namespace orrery {
-
 namespace daffile {
+
+/*
+
+
+
+*/
 
 const size_t block_size = 1024;
 
-void SpkSegment::compute(double jd, OrreryBody& body)
-{
-}
+// void SpkSegment::compute(double jd, OrreryBody& body)
+// {
+// }
 
-const std::string ftp_validation_template = "FTPSTR:\r:\n:\r\n:\r\x00:\x81:\x10\xce:ENDFTP";
-
+const std::string ftp_validation_template
+    = "FTPSTR:\r:\n:\r\n:\r\x00:\x81:\x10\xce:ENDFTP";
 
 // There is one file record at the start that has a specific structure reflected
 // here
 struct FileRecord {
-    char      file_architecture[8]; // LOCIDW
-    u_int32_t n_double_precision; // ND
-    u_int32_t n_integers; // NI
-    char      internal_name[60]; // LOCIFN
-    u_int32_t n_initial_summary; // FWARD
-    u_int32_t n_final_summary; // BWARD
-    u_int32_t first_free_address; // FREE
-    char      numeric_format[8]; // LOCFMT
-    char      zeros_1[603]; //PRENUL
-    char      ftp_validation_str[28]; //FTPSTR
-    char      zeros_2[297]; //PSTNUL
-};
+    char      file_architecture[8];   // LOCIDW
+    u_int32_t n_double_precision;     // ND
+    u_int32_t n_integers;             // NI
+    char      internal_name[60];      // LOCIFN
+    u_int32_t n_initial_summary;      // FWARD
+    u_int32_t n_final_summary;        // BWARD
+    u_int32_t first_free_address;     // FREE
+    char      numeric_format[8];      // LOCFMT
+    char      zeros_1[603];           // PRENUL
+    char      ftp_validation_str[28]; // FTPSTR
+    char      zeros_2[297];           // PSTNUL
+} __attribute__((__packed__));
 
 bool read_file_record(std::ifstream& nasa_spk_file, FileRecord& hdr)
 {
@@ -62,6 +72,12 @@ bool read_file_record(std::ifstream& nasa_spk_file, FileRecord& hdr)
     LOG_S(INFO) << "File architecture: " << hdr.file_architecture;
     LOG_S(INFO) << "Internal name: " << hdr.internal_name;
     LOG_S(INFO) << "Numeric format: " << hdr.numeric_format;
+
+    if ((hdr.n_double_precision != 2) & (hdr.n_integers != 6)) {
+        LOG_S(ERROR) << "Expected ND = 2, NI = 6, but got "
+                     << hdr.n_double_precision << ", " << hdr.n_integers;
+        return false;
+    }
 
     if (ftp_validation_template.compare(hdr.ftp_validation_str) != 0) {
         LOG_S(ERROR) << "This file is likely corrupted: FTP validation fails";
@@ -74,7 +90,8 @@ bool read_file_record(std::ifstream& nasa_spk_file, FileRecord& hdr)
 // 1024 ("block_size") byte blocks are the fundamental unit of a DAF record
 // Summary records point to record block numbers ("n_block") that we can use
 // to jump to particular records
-void read_record_block(std::ifstream& nasa_spk_file, size_t n_block, char buf[block_size])
+void read_record_block(
+    std::ifstream& nasa_spk_file, size_t n_block, char buf[block_size])
 {
     nasa_spk_file.seekg(block_size * (n_block - 1));
     nasa_spk_file.read(buf, block_size);
@@ -100,7 +117,8 @@ void parse_daf_comment(char a[block_size])
     }
 }
 
-std::string read_comment_blocks(std::ifstream& nasa_spk_file, size_t n_initial_summary)
+std::string
+read_comment_blocks(std::ifstream& nasa_spk_file, size_t n_initial_summary)
 {
     std::string comment;
     for (int i = 2; i < n_initial_summary; i++) {
@@ -112,53 +130,94 @@ std::string read_comment_blocks(std::ifstream& nasa_spk_file, size_t n_initial_s
     return comment;
 }
 
-size_t read_segment_record(std::ifstream& nasa_spk_file, size_t segment_start_block)
+// Summary record header.
+// These are integers but they are stored as doubles
+struct SummaryRecord {
+    double n_next_summary_record;     // pointer to next SR
+    double n_previous_summary_record; // pointer to previous SR
+    double n_element_records;         // number of element summaries here
+} __attribute__((__packed__));
+
+// Summaries
+struct ElementSummary {
+    double    start_second; // initial epoch, as seconds from J2000
+    double    end_second;   // final epoch, as seconds from J2000
+    int       target;       // integer target identifier
+    int       center;       // integer center identifier
+    int       frame;        // integer frame identifier
+    int       data_type;    // integer data type identifier
+    u_int32_t start_i;      // index where segment starts
+    u_int32_t end_i;        // index where segment ends
+} __attribute__((__packed__));
+
+std::ostream& operator<<(std::ostream& out, const ElementSummary& es)
 {
-  char buf[block_size];
-  read_record_block(nasa_spk_file, segment_start_block, buf);
-  double next_segment_start_block = *(double*) &buf[0];
-  double n_blocks_in_segment = *(double*) &buf[16];
-
-
-
-  return (size_t) next_segment_start_block;
+    out << "(" << es.start_second << " - " << es.end_second
+        << ": T:" << es.target << " C:" << es.center << ")";
+    return out;
 }
 
-//TODO: Handle files of both endian-ness
-bool SpkOrrery::load_orrery_model(std::string fname)
-{
-    std::ifstream nasa_spk_file(fname, std::ios::binary);
+typedef std::vector<ElementSummary> ElementSummaryVec;
 
-    if (!nasa_spk_file) {
-        LOG_S(ERROR) << "Could not open " << fname;
-        ok = false;
-        return false;
+ElementSummaryVec
+read_summaries(std::ifstream& nasa_spk_file, size_t segment_start_block)
+{
+    SummaryRecord     sr;
+    ElementSummaryVec esv;
+
+    size_t summary_records_read = 0;
+
+    nasa_spk_file.seekg(block_size * (segment_start_block - 1));
+    nasa_spk_file.read((char*)&sr, sizeof(sr));
+
+    for (;;) {
+        LOG_S(INFO) << "Read summary record #" << ++summary_records_read;
+        for (size_t n_element = 0; n_element < sr.n_element_records;
+             n_element++) {
+            ElementSummary es;
+            nasa_spk_file.read((char*)&es, sizeof(es));
+            esv.push_back(es);
+            LOG_S(INFO) << "Read element summary " << n_element + 1 << "/"
+                        << sr.n_element_records << " " << es;
+        }
+
+        if (sr.n_next_summary_record == 0)
+            break;
+
+        nasa_spk_file.seekg(block_size * (sr.n_next_summary_record - 1));
+        nasa_spk_file.read((char*)&sr, sizeof(sr));
     }
+
+    return ElementSummaryVec();
+}
+
+// size_t read_summary_record(std::ifstream& nasa_spk_file, size_t
+// segment_start_block)
+// {
+//     char buf[block_size];
+//     read_record_block(nasa_spk_file, segment_start_block, buf);
+//     double next_segment_start_block = *(double*)&buf[0];
+//     double n_blocks_in_segment      = *(double*)&buf[16];
+
+//     return (size_t)next_segment_start_block;
+// }
+
+struct EphemerisCoeffcients {
+};
+
+void load_spk(std::ifstream& nasa_spk_file)
+{
 
     FileRecord hdr;
     if (!read_file_record(nasa_spk_file, hdr)) {
-        ok = false;
-        return false;
+        // ok = false;
+        // return false;
     }
 
-    std::string comment = read_comment_blocks(nasa_spk_file, hdr.n_initial_summary);
-    //std::cout << comment;
+    std::string comment
+        = read_comment_blocks(nasa_spk_file, hdr.n_initial_summary);
+    // std::cout << comment;
 
-    ok = true;
-    return ok;
-}
-
-// Fill out the (x, y, z) of each Orrery body and return us an immutable
-// vector containing this information.
-const OrreryBodyVec& SpkOrrery::get_orrery_at(double jd)
-{
-    for (int i = 0; i < bodies.size(); i++) {
-        segments[i].compute(jd, bodies[i]);
-        if (segments[i].center != 0) {
-            bodies[i].pos += bodies[segments[i].center_i].pos;
-        }
-    }
-    return bodies;
-}
+    read_summaries(nasa_spk_file, hdr.n_initial_summary);
 }
 }
--- src/orrery/spk.hpp
+++ src/orrery/spk.hpp
@@ -1,13 +1,7 @@
 /*
-  Code to read NASA SPK files. The aim is to have a complete interface that
-  allows us to load a NASA SPK file and then query it for trajectory data
-  at arbitrary time points. We can have multiple SPK files loaded at the same
-  time: the code gives us a central interface that allows us to query
-  trajectories by name without having to keep track of which SPK file the data
-  was originally in.
-
-  Order of the SPK files matter if a body is duplicated. In all cases the last
-  loaded body will be the one fetched.
+  Code to read NASA SPK files. The aim is to have a minimal interface that
+  allows us to load element data from a NASA SPK file and return it as a
+  self-contained structure.
 */
 #pragma once
 
@@ -17,46 +11,16 @@
 
 #include "orrery.hpp"
 
-namespace orrery {
-
 namespace daffile {
 
-struct SpkSegment {
-    std::string source; // official ephemeris name, like 'DE-0430LE-0430'
-
-    double start_second; // initial epoch, as seconds from J2000
-    double end_second; // final epoch, as seconds from J2000
-    double start_jd; // start_second, converted to a Julian Date
-    double end_jd; // end_second, converted to a Julian Date
-    int    center; // integer center identifier
-    int    target; // integer target identifier
-    size_t center_i; // index of segment/body refered as center
-    int    frame; // integer frame identifier
-    int    data_type; // integer data type identifier
-    size_t start_i; // index where segment starts
-    size_t end_i; // index where segment ends
-
-    // Given a julian date fill out (x, y, z) for the passed OrreryBody
-    void compute(double jd, OrreryBody& body);
+struct Emphemeris {
+    std::string target_name; // Human readable name for this body
+    std::string center_name; // Human readable name for reference body
+    int         target_code; // NASA/JPL code for this body
+    int         center_code; // NASA/JPL code for reference body
+    double      jd_start;    // At what epoch does this emphemeris start
+    double      jd_stop;     // At what epoch does this emphemeris end
 };
 
-typedef std::vector<SpkSegment> SpkSegmentVec;
-
-class SpkOrrery : public Orrery {
-
-    SpkSegmentVec segments;
-
-public:
-SpkOrrery() : Orrery() {}
-    // Load SPK file "fname" and make the bodies in it available to us
-    // Calling this repeatedly will load multiple SPK files.
-    // Bodies that appear multiple times will be over written such that
-    // only the last read version is kept
-    bool load_orrery_model(std::string fname);
-
-    // Fill out the (x, y, z) of each Orrery body and return us an immutable
-    // vector containing this information.
-    const OrreryBodyVec& get_orrery_at(double jd);
-};
-}
+void load_spk(std::ifstream& nasa_spk_file);
 }
