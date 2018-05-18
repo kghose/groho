5e2d68dfd6901a7959029cd80d89d16a0c6e93bb
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-05-18 17:18:33 -0400

    Add utility functions for text processing and file modification time
    Text processing functions are used to parse flight plan lines.
    File modification time is required to make file reloads smarter.

--- src/scenario/scenariolib.hpp
+++ src/scenario/scenariolib.hpp
@@ -6,8 +6,27 @@ This file contains some common functions used in parsing scenario files
 */
 #pragma once
 
+#include <iterator>
 #include <optional>
+#include <sstream>
 #include <string>
+#include <vector>
+
+// For file modification time
+#include <sys/stat.h>
+#include <sys/types.h>
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#ifdef WIN32
+#define stat _stat
+#endif
+
+#include "vector.hpp"
+
+#define LOGURU_WITH_STREAMS 1
+#include "loguru.hpp"
 
 namespace sim {
 
@@ -41,5 +60,47 @@ inline std::optional<KeyValue> get_key_value(std::string line)
     kv.key   = trim_whitespace(line.substr(0, n0 - 1));
     kv.value = trim_whitespace(line.substr(n0 + 1));
     return kv;
+}
+
+inline std::vector<std::string> split(std::string line)
+{
+    std::istringstream iss(line);
+    return std::vector<std::string>(
+        std::istream_iterator<std::string>(iss),
+        std::istream_iterator<std::string>());
+}
+
+inline Vector stoV(std::string v)
+{
+    std::vector<std::string> tokens = split(v);
+    if (tokens.size() < 3) {
+        LOG_S(ERROR) << "Vector needs three components: Got " << v;
+        return {};
+    }
+    Vector out{ stof(tokens[0]), stof(tokens[1]), stof(tokens[2]) };
+    return out;
+}
+
+inline time_t file_modification_time(std::string fname)
+{
+    struct stat result;
+    if (stat(fname.c_str(), &result) == 0) {
+        return result.st_mtime;
+    } else {
+        return 0;
+    }
+}
+
+// Given a list of files, give us the time of latest change among them all
+inline time_t file_modification_time(std::vector<std::string> fnames)
+{
+    time_t fmod_time = 0;
+    for (auto& fn : fnames) {
+        time_t fn_ft = file_modification_time(fn);
+        if (fn_ft > fmod_time) {
+            fmod_time = fn_ft;
+        }
+    }
+    return fmod_time;
 }
 }
