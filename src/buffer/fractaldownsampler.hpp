09aa69b5fe7499acde868d7e3759e80fde80348f
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-05-22 22:19:40 -0400

    Run clang-format on this code. No syntax changes.

--- src/buffer/fractaldownsampler.hpp
+++ src/buffer/fractaldownsampler.hpp
@@ -1,5 +1,5 @@
 /*
-  This handles the downsampling of the data 
+  This handles the downsampling of the data
 */
 #pragma once
 #include "vector.hpp"
@@ -7,41 +7,39 @@
 #define LOGURU_WITH_STREAMS 1
 #include "loguru.hpp"
 
+namespace sim {
 
-namespace sim
-{
-
-class FractalDownsampler
-{
+class FractalDownsampler {
 public:
-  FractalDownsampler( double rt=1.001, double lt=1e6 ) : 
-      ratio_threshold( rt ), linear_threshold( lt )
-  {}
-
-  bool operator()( const Vector& v )
-  {
-    cumulative_curve_dist += (v - last_v).norm();
-    float linear_dist = (v - last_sample_v).norm();
-     
-    if(  ( ( cumulative_curve_dist / linear_dist )    > ratio_threshold  )   
-       | ( abs( cumulative_curve_dist - linear_dist ) > linear_threshold ) ) 
+    FractalDownsampler(double rt = 1.001, double lt = 1e6)
+        : ratio_threshold(rt)
+        , linear_threshold(lt)
     {
-      //DLOG_S(INFO) << "HI" << cumulative_curve_dist;
-      cumulative_curve_dist = 0;
-      last_sample_v = v;
-      last_v = v;
-      return true;
     }
-    last_v = v;
-    return false;
-  }
+
+    bool operator()(const Vector& v)
+    {
+        cumulative_curve_dist += (v - last_v).norm();
+        float linear_dist = (v - last_sample_v).norm();
+
+        if (((cumulative_curve_dist / linear_dist) > ratio_threshold)
+            | (abs(cumulative_curve_dist - linear_dist) > linear_threshold)) {
+
+            cumulative_curve_dist = 0;
+            last_sample_v         = v;
+            last_v                = v;
+            return true;
+        }
+        last_v = v;
+        return false;
+    }
 
 private:
-  double     cumulative_curve_dist = 0;
-  Vector     last_sample_v = { 0, 0, 0 };
-  Vector     last_v = { 0, 0, 0 };
-  double     ratio_threshold = 1.001;
-  double     linear_threshold = 1e6;
+    double cumulative_curve_dist = 0;
+    Vector last_sample_v         = { 0, 0, 0 };
+    Vector last_v                = { 0, 0, 0 };
+    double ratio_threshold       = 1.001;
+    double linear_threshold      = 1e6;
 };
 
 } // namespace sim
