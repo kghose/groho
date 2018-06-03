472b3cebb3689cbaa990ffbbe392b1c240c4ffa8
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-06-02 20:20:20 -0400

    Put gloabls in own header file

---
+++ src/globals.hpp
@@ -0,0 +1,12 @@
+/*
+This file is part of Groho, a simulator for inter-planetary travel and warfare.
+Copyright (c) 2017-2018 by Kaushik Ghose. Some rights reserved, see LICENSE
+
+Convenient collecting point for some program wide constants
+*/
+
+namespace sim {
+namespace globals {
+const float scale = 10 * 149'597'870.700; // 10 AU
+}
+}
