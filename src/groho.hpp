966a6ce43012be2252b2c2aa71037612215cbdb1
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-06-14 10:54:42 -0400

    refactor: Add special type for spkid
    Also rename globals.hpp to groho.hpp to mark that this
    file collects not only globals but program wide definitions

--- src/groho.hpp
+++ src/groho.hpp
@@ -2,11 +2,19 @@
 This file is part of Groho, a simulator for inter-planetary travel and warfare.
 Copyright (c) 2017-2018 by Kaushik Ghose. Some rights reserved, see LICENSE
 
-Convenient collecting point for some program wide constants
+Convenient collecting point for some program wide definitions and constants
 */
 #pragma once
 
 namespace sim {
+
+// I wanted a way to distinguish functions that took an index into an array
+// and functions that actually take a spk_id.
+struct spkid_t {
+    int id;
+    spkid_t(int a) { id = a; }
+};
+
 namespace globals {
 const float scale = 10 * 149'597'870.700; // 10 AU
 }
