2548ec82117b6017d8778d4d772267aeb1317669
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-06-19 23:15:58 -0400

    Add state file (Part of Restructure Scenario PT I)

---
+++ src/simulator/state.hpp
@@ -0,0 +1,31 @@
+/*
+This file is part of Groho, a simulator for inter-planetary travel and warfare.
+Copyright (c) 2018 by Kaushik Ghose. Some rights reserved, see LICENSE
+
+This class collects Orrery, Spaceships and other object state at a given instant
+*/
+
+#pragma once
+
+#include "body.hpp"
+
+namespace sim {
+
+struct State {
+
+    State(double t_s, const std::vector<Body>& orrery, std::vector<Body>& ships)
+        : t_s(t_s)
+        , orrery(orrery)
+        , ships(ships)
+    {
+    }
+
+    double t_s;
+
+    // This points to the state inside the Orrery itself
+    const std::vector<Body>& orrery;
+
+    // This points to the ship structure in the Scenario
+    std::vector<Body>& ships;
+};
+} // sim
