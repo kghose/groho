7ea659320c9a533de3fed756534423da14ca19b9
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-06-23 22:53:11 -0400

    wip: Working on "park" action

--- src/scenario/flightplanaction.cpp
+++ src/scenario/flightplanaction.cpp
@@ -30,102 +30,14 @@ namespace sim {
 
 double do_this_first(std::numeric_limits<double>::lowest());
 
-///////////////////////////////
-// AVAILABLE ACTIONS (VERBS) //
-///////////////////////////////
-
-/*
-
-
-// Utility to compute deltaV given GM, R and V
-Vector parking_deltaV(double GM, Vector R, Vector Vs)
-{
-    double r_mod       = R.norm();
-    Vector minus_R_hat = R.normed() * -1;
-    Vector Vs_hat      = Vs.normed();
-    Vector Uo_hat      = cross(minus_R_hat, Vs_hat);
-    Vector Vo_hat      = cross(Uo_hat, minus_R_hat);
-    Vector Vo          = Vo_hat * std::sqrt(GM / r_mod);
-    return Vo - Vs;
-}
-
-// V0.0
-// Once we come within R km of a planet, maneuver to set V to match orbital VO
-// For each step
-// 1. Compute the velocity vector required to convert V to VO (deltaV)
-// 2. Set attitude along deltaV
-// 3. Set engine accel (A) such that deltaV = 0.5 * A * Tstep^2 or maxA
-// 4. Once |V| is within epsilon (e) of |VO| deactivate this program
-struct PARK_IN_ORBIT : public FlightPlanAction {
-    const double epsilon = 0.001; // 1m/s tolerance
-    const double R_capture;
-    const int    spkid;
-    size_t       obv_idx
-        = -1; // The index into the ws.obv vector for body with given spkid
-    bool parking_maneuver_started = false;
-
-    PARK_IN_ORBIT(double jd, size_t line_no, int spkid, double R)
-        : FlightPlanAction(jd, line_no)
-        , R_capture(R)
-        , spkid(spkid)
-    {
-    }
-
-    void operator()(Body& body, const WorldState& ws)
-    {
-        if (obv_idx == -1) {
-            obv_idx = orrery::spkid_to_orrery_index(ws.obv, spkid);
-            if (obv_idx == -1) {
-                LOG_S(ERROR) << line_no << ": PARK_IN_ORBIT : Can't find "
-                             << spkid << " in Orrery";
-                active = false;
-                return;
-            }
-        }
-
-        auto const& ob = ws.obv[obv_idx];
-
-        Vector R = body.state.pos - ob.state.pos;
-        // DLOG_S(INFO) << R.norm();
-        if (R.norm() > R_capture) {
-            return;
-        }
-
-        if (!parking_maneuver_started) {
-            LOG_S(INFO) << line_no << ": Starting to park " << body.name
-                        << " -> " << ob.name;
-            parking_maneuver_started = true;
-        }
-
-        Vector deltaV = parking_deltaV(
-            ob.GM, body.state.pos - ob.state.pos, body.state.vel);
-
-        body.state.att = deltaV.normed();
-        // deltaV = 0.5 * A * Tstep^2
-        double acc = deltaV.norm() / (0.5 * ws.t_s * ws.t_s);
-
-        if (acc > body.max_acc) {
-            body.state.acc = body.max_acc;
-            LOG_S(WARNING) << line_no << ": acceleration required for parking "
-                           << body.name << " exceeds max_acc";
-        } else {
-            body.state.acc = acc;
-        }
-
-        if (deltaV.norm() < epsilon) {
-            active = false;
-            LOG_S(INFO) << line_no << ": Ship " << body.name
-                        << " successfully parked around " << ob.name;
-        }
-    }
-};*/
-
-///////////////////////////////
-
 typedef std::string                      str_t;
 typedef std::vector<std::string>         strv_t;
 typedef std::unordered_map<str_t, str_t> params_t;
 typedef FlightPlanAction                 fpa_t;
+
+///////////////////////////////
+// AVAILABLE ACTIONS (VERBS) //
+///////////////////////////////
 
 // Initializer actions, should only be allowed at start of simulation ////////
 
@@ -173,6 +85,7 @@ struct INITIAL_ORBIT : public FlightPlanAction {
             LOG_S(ERROR)
                 << "Can't find " << spk_id
                 << " in Orrery when trying to set initial state as orbiting";
+            done = true;
             return;
         }
 
@@ -199,7 +112,7 @@ struct INITIAL_ORBIT : public FlightPlanAction {
     double alt;
 };
 
-// Basic Actions ///////////////////////////////////////////////////////////
+// Basic Actions ///////////////////////////////////////////////////////////////
 
 struct SET_ATTITUDE : public FlightPlanAction {
 
@@ -228,6 +141,9 @@ struct SET_ATTITUDE : public FlightPlanAction {
 
     void operator()(State& state)
     {
+        if (state.t_s < p.t_s)
+            return;
+
         state.ships[p.ship_idx].state.att = att;
         DLOG_S(INFO) << state.ships[p.ship_idx].property.name
                      << " attitude set";
@@ -259,6 +175,9 @@ struct SET_ACCEL : public FlightPlanAction {
 
     void operator()(State& state)
     {
+        if (state.t_s < p.t_s)
+            return;
+
         double _acc = acc;
         if (_acc > state.ships[p.ship_idx].property.max_acc) {
             _acc = state.ships[p.ship_idx].property.max_acc;
@@ -276,10 +195,125 @@ struct SET_ACCEL : public FlightPlanAction {
     double acc;
 };
 
+// Advanced actions ////////////////////////////////////////////////////////////
+
+// Utility to compute deltaV given GM, R and V
+Vector parking_deltaV(double GM, Vector R, Vector Vs)
+{
+    double r_mod       = R.norm();
+    Vector minus_R_hat = R.normed() * -1;
+    Vector Vs_hat      = Vs.normed();
+    Vector Uo_hat      = cross(minus_R_hat, Vs_hat);
+    Vector Vo_hat      = cross(Uo_hat, minus_R_hat);
+    Vector Vo          = Vo_hat * std::sqrt(GM / r_mod);
+    return Vo - Vs;
+}
+
+// V0.0
+// Once we come within R km of a planet, maneuver to set V to match orbital VO
+// For each step
+// 1. Compute the velocity vector required to convert V to VO (deltaV)
+// 2. Set attitude along deltaV
+// 3. Set engine accel (A) such that deltaV = 0.5 * A * Tstep^2 or maxA
+// 4. Once |V| is within epsilon (e) of |VO| deactivate this program
+struct PARK_IN_ORBIT : public FlightPlanAction {
+    const double          epsilon = 0.001; // 1m/s tolerance
+    double                R_capture;
+    std::optional<double> last_t_s;
+    int                   spk_id;
+    size_t                obv_idx
+        = -1; // The index into the ws.obv vector for body with given spkid
+    bool parking_maneuver_started = false;
+
+    PARK_IN_ORBIT(const FPAD& _fpad)
+        : FlightPlanAction(_fpad)
+    {
+    }
+
+    static ptr_t<PARK_IN_ORBIT> construct(const FPAD& _fpad, params_t params)
+    {
+        try {
+            auto action = ptr_t<PARK_IN_ORBIT>(new PARK_IN_ORBIT(_fpad));
+
+            action->spk_id    = stof(params["id"]);
+            action->R_capture = stof(params["r"]);
+            return action;
+        } catch (std::exception) {
+            LOG_S(ERROR)
+                << _fpad.fname << ": Line: " << _fpad.line_no
+                << ": Park in orbit needs two elements eg: id:399 r:200";
+            return {};
+        }
+    }
+
+    void operator()(State& state)
+    {
+        if (obv_idx == -1) {
+            obv_idx = orrery::spkid_to_orrery_index(state.orrery, spk_id);
+            if (obv_idx == -1) {
+                LOG_S(ERROR) << p.line_no << ": park : Can't find " << spk_id
+                             << " in Orrery";
+                done = true;
+                return;
+            }
+        }
+
+        auto const& ob   = state.orrery[obv_idx];
+        auto&       ship = state.ships[p.ship_idx];
+
+        // If this is the first time we call this we need to gather more state
+        // information
+        if (!last_t_s) {
+            last_t_s = ship.state.t;
+            return;
+        }
+
+        Vector R = ship.state.pos - ob.state.pos;
+        if (R.norm() > R_capture) {
+            return;
+        }
+
+        if (!parking_maneuver_started) {
+            DLOG_S(INFO) << p.fname << ": Line " << p.line_no
+                         << ": Starting to park " << ship.property.name
+                         << " -> " << ob.property.name;
+            parking_maneuver_started = true;
+        }
+
+        Vector deltaV = parking_deltaV(
+            ob.property.GM, ship.state.pos - ob.state.pos, ship.state.vel);
+
+        ship.state.att = deltaV.normed();
+        // deltaV = 0.5 * A * Tstep^2
+        double Tstep = state.t_s - *last_t_s;
+        *last_t_s    = state.t_s;
+        double acc   = deltaV.norm() / (0.5 * Tstep * Tstep);
+
+        if (acc > ship.property.max_acc) {
+            ship.state.acc = ship.property.max_acc;
+            LOG_S(WARNING)
+                << p.fname << ": Line " << p.line_no
+                << ": acceleration required for parking exceeds max_acc";
+        } else {
+            ship.state.acc = acc;
+        }
+
+        if (deltaV.norm() < epsilon) {
+            done = true;
+            LOG_S(INFO) << p.fname << ": Line " << p.line_no << ": Ship "
+                        << ship.property.name << " successfully parked around "
+                        << ob.property.name;
+        }
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
 std::unordered_map<str_t, std::function<fpap_t(const FPAD&, params_t&)>>
     available_actions{ { "set-attitude", SET_ATTITUDE::construct },
                        { "set-accel", SET_ACCEL::construct },
-                       { "initial-orbit", INITIAL_ORBIT::construct } };
+                       { "initial-orbit", INITIAL_ORBIT::construct },
+                       { "park", PARK_IN_ORBIT::construct } };
 
 fpap_t parse_line_into_action(
     size_t                   ship_idx,
@@ -303,7 +337,7 @@ fpap_t parse_line_into_action(
         }
 
         return available_actions[tokens[1]](
-            FPAD{ ship_idx, fname, line_no, jd }, params);
+            FPAD{ ship_idx, fname, line_no, jd2s(jd) }, params);
 
     } else {
         LOG_S(ERROR) << fname << ": " << line_no
