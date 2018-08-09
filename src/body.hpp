8dbcffd7f51e9025e2c7b5b96cbf81dfd47e6b31
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-08-08 22:25:58 -0400

    refactor: Separate out Rock and Ship
    Instead of having one struct which is a union of the
    characteristics we need for Rocks and Ships, we
    split them into separate objects.
    
    In the simulator we were already considering
    these two objects spearately even though we had a single
    array storing the common type. We has to come up with
    an awkward way of separating the two classes (by reserving
    the first part of the vector for Rocks and the second
    part for Ships).
    
    In the display it is much simpler, however, to treat these
    as homogenous objects to be displayed. This change should
    not make the conversion from simulation to display any more
    complex than it already is, with the added benefit we no
    longer have the awkward indexing trick, and we can give
    the display access to separate Rock and Ship properties
    which will come in useful in case we want a more detailed
    display (labeling) later.

--- src/body.hpp
+++ src/body.hpp
@@ -3,12 +3,9 @@ This file is part of Groho, a simulator for inter-planetary travel and warfare.
 Copyright (c) 2017-2018 by Kaushik Ghose. Some rights reserved, see LICENSE
 
 We have two types of objects in the simulation - Orrery bodies
-(planets/moons/asteroid) and spaceships. They have some metadata in common and
-some that are unique.
+(planets/moons/asteroid) and spaceships. They behave quite differently and
+are described as separate classes of objects with some elements in common
 
-To simplify our first iteration we combine all the metadata and state
-information and use a unifed system with a type flag to indicate what type of
-object we are refering to. This determines which fields make sense to access.
 */
 
 #pragma once
@@ -18,55 +15,57 @@ object we are refering to. This determines which fields make sense to access.
 #include <string>
 #include <vector>
 
-#include "spkid.hpp"
+#include "naifbody.hpp"
 #include "vector.hpp"
 
 namespace sim {
 
-enum BodyType { ROCK, BARYCENTER, SPACESHIP };
-enum FlightState { FALLING, LANDED };
+struct Rock {
 
-struct BodyConstant {
-    BodyType    body_type;
-    int         code; // SPK code for body (made up ones for spaceships)
-    std::string name; // Human readable name for body
+    struct Property {
+        NAIFbody naif;
 
-    float GM; // GM value for body
-    float r;  // Radius of body (for collision tests)
+        float GM; // GM value for body
+        float r;  // Radius of body (for collision tests)
 
-    uint32_t color; // For display purposes
+        uint32_t color; // For display purposes
+    } property;
+
+    struct State {
+        Vector pos; // Position referenced to solar system barycenter
+        Vector vel; // Velocity
+        double t_s;
+    } state;
 };
 
-struct ShipCharacteristic {
-    float max_acc;   // Maximum acceleration possible for ship m/s^2
-    float max_fuel;  // Maximum fuel reserve (U)
-    float burn_rate; // Fuel consumption rate (U/ (m/s^2))
+struct Ship {
+
+    struct Property {
+        NAIFbody naif;
+
+        float max_acc;   // Maximum acceleration possible for ship m/s^2
+        float max_fuel;  // Maximum fuel reserve (U)
+        float burn_rate; // Fuel consumption rate (U/ (m/s^2))
+
+        uint32_t color; // For display purposes
+    } property;
+
+    struct State {
+        Vector pos;  // Position referenced to solar system barycenter
+        Vector vel;  // Velocity
+        Vector att;  // Attitude
+        float  fuel; // Current fuel reserve (U)
+        float  acc;  // Current acceleration km/s^2
+        double t_s;
+    } state;
 };
 
-struct BodyState {
-    double t;
-
-    Vector pos; // Position referenced to solar system barycenter
-    Vector vel; // Velocity
-    Vector att; // Attitude
-
-    float acc;  // Current acceleration km/s^2
-    float fuel; // Current fuel reserve (U)
-
-    FlightState flight_state;
-};
-
-struct Body {
-    BodyConstant       property;
-    ShipCharacteristic param;
-    BodyState          state;
-};
-
+template <typename T>
 inline std::optional<size_t>
-body_index(const std::vector<Body>& bodies, spkid_t spkid)
+find(const std::vector<T>& bodies, const NAIFbody& naif)
 {
     for (size_t i = 0; i < bodies.size(); i++) {
-        if (bodies[i].property.code == spkid.id) {
+        if (bodies[i].naif == naif) {
             return i;
         }
     }
