cd415cb0d57eb9ada537152d29b92a8d73ab0898
Kaushik Ghose <kaushik.ghose@gmail.com>
2018-08-20 21:24:13 +0200

    wip: Version 1 of template gymnastics
    The specialization with template does not compile. I will
    try without inheritance next.

--- src/body.hpp
+++ src/body.hpp
@@ -67,6 +67,84 @@ template <typename T> struct SnapShot {
 
 // This allows us to do lazy computation of rock velocities.
 // Time will tell if we were too clever here
+template <typename T> struct SnapShotV {
+
+    SnapShotV(int& N, typename T::Property _property)
+        : _N(N)
+    {
+        property = _property;
+    }
+
+    typename T::Property property;
+    typename T::State    _state[2];
+
+    double& t_s() { return _state[_N].t_s; }
+    double  t_s() const { return _state[_N].t_s; }
+
+    Vector&       pos() { return _state[_N].pos; }
+    const Vector& pos() const { return _state[_N].pos; }
+    Vector        vel() const
+    {
+        return (_state[_N].pos - _state[1 - _N].pos)
+            / (_state[_N].t_s - _state[1 - _N].t_s);
+    }
+
+    const typename T::State& state() const { return _state[_N]; }
+
+    int& _N;
+};
+
+template <typename T> struct BaseCollection {
+    std::vector<T>                       bodies;
+    std::unordered_map<NAIFbody, size_t> lookup;
+
+    size_t size() const { return bodies.size(); }
+
+    T& operator[](size_t idx) { return bodies[idx]; }
+    T& operator[](const NAIFbody& id) { return bodies[lookup[id]]; }
+
+    const T& operator[](size_t idx) const { return bodies[idx]; }
+    const T& operator[](const NAIFbody& id) const { return bodies[lookup[id]]; }
+
+    virtual void push_back(const T& body)
+    {
+        bodies.push_back(body);
+        lookup[body.property.naif] = bodies.size() - 1;
+    }
+};
+
+template <typename T> struct Collection : public BaseCollection<T> {
+};
+
+// template <>
+template <typename T>
+struct Collection<SnapShotV<T>> : public BaseCollection<SnapShotV<T>> {
+
+    int N = 0;
+
+    void push_back(const typename T::Property& property)
+    {
+        // using BaseCollection<SnapShotV<T>>::bodies;
+        // using BaseCollection<SnapShotV<T>>::lookup;
+
+        bodies.push_back({ N, property });
+        lookup[property.naif] = bodies.size() - 1;
+    }
+
+private:
+    using BaseCollection<SnapShotV<T>>::push_back;
+};
+
+template <template <typename> class Trock, template <typename> class Tship>
+struct RocksAndShips {
+    Collection<Trock<RockLike>> system;
+    Collection<Tship<ShipLike>> fleet;
+};
+
+/*
+// TODO: deprecate
+// This allows us to do lazy computation of rock velocities.
+// Time will tell if we were too clever here
 struct RockSnapShotWithVel {
 
     RockSnapShotWithVel(int& N, RockLike::Property _property)
@@ -135,25 +213,7 @@ template <template <typename> class T> struct RocksAndShips {
     Collection<T<RockLike>> system;
     Collection<T<ShipLike>> fleet;
 };
-
-// struct RocksAndShipsSnapShotWithVel {
-
-//     int N = 0;
-
-//     Collection<RockSnapShotWithVel> system;
-//     Collection<SnapShot<ShipLike>>  fleet;
-
-//     RocksAndShipsSnapShotWithVel() = default;
-// };
-
-// TODO: DEPRECATE THIS
-template <template <typename> class T> struct Objects {
-    std::vector<T<RockLike>> system;
-    std::vector<T<ShipLike>> fleet;
-
-    std::unordered_map<NAIFbody, size_t> lookup_system;
-    std::unordered_map<NAIFbody, size_t> lookup_fleet;
-};
+*/
 
 template <typename T>
 inline std::optional<size_t>
