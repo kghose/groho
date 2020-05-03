59ed23c41280c7bbb4ba53880a5a06956580ca75
Kaushik Ghose <kaushik.ghose@gmail.com>
2020-05-02 23:14:03 -0400

    Rewriting CLI with new multi-command interface

--- src/main.cpp
+++ src/main.cpp
@@ -6,95 +6,46 @@ LICENSE
 Entry point function for command line program
 */
 
-#include <chrono>
-#include <filesystem>
-#include <iostream>
-#include <thread>
-
-#include <signal.h>
-#include <stdlib.h>
-
 #include "CLI11.hpp"
-
-#include "simulator.hpp"
-
-#define LOGURU_IMPLEMENTATION 1
-#define LOGURU_WITH_STREAMS 1
-#include "loguru.hpp"
 
 void print_license()
 {
     std::cout << R"(  
-    Groho 20.02: A simulator for inter-planetary travel and warfare
+    Groho 20.05: A simulator for inter-planetary travel and warfare
     Copyright (c) 2017 - 2020 by Kaushik Ghose. 
     Released under the MIT License. Some rights reserved.
     
 )";
 }
 
-struct Options {
-    std::string scenario_file;
-    std::string output_folder;
-};
-
-Options parse_arguments(int argc, char* argv[])
-{
-    bool        show_actions_and_exit;
-    std::string scenario_file, output_folder;
-
-    CLI::App cli_options{ "Groho: A simulator for inter-planetary travel" };
-    cli_options.add_flag(
-        "--actions", show_actions_and_exit, "Show action list");
-    cli_options.add_option("scenariofile", scenario_file, "Scenario file");
-    cli_options.add_option("outputfolder", output_folder, "Output folder");
-
-    try {
-        cli_options.parse(argc, argv);
-    } catch (const CLI::ParseError& e) {
-        exit(cli_options.exit(e));
-    }
-
-    if (show_actions_and_exit) {
-        std::cout << groho::Simulator::list_available_actions();
-        exit(0);
-    }
-
-    if (scenario_file == "" | output_folder == "") {
-        std::cout << cli_options.help("", CLI::AppFormatMode::All);
-        exit(0);
-    }
-
-    return Options{ scenario_file, output_folder };
-}
-
-volatile sig_atomic_t keep_running = true;
-
-void ctrl_c_pressed(int) { keep_running = false; }
-
 int main(int argc, char* argv[])
 {
     print_license();
 
-    Options opt = parse_arguments(argc, argv);
+    CLI::App app{ "Groho: A simulator for inter-planetary travel" };
+    app.require_subcommand(1);
 
-    loguru::init(argc, argv);
+    std::string scn_file, plot_file, sim_folder, chart_pdf;
 
-    // This has to come after loguru::init
-    signal(SIGINT, ctrl_c_pressed);
+    auto loop = app.add_subcommand(
+        "loop",
+        "Monitor changes in scenario and plot files,\n"
+        "rerun and rechart simulation continuously");
+    loop->add_option("simfile", scn_file, "Scenario file");
+    loop->add_option("plotfile", plot_file, "Plot file");
+    loop->add_option("simfolder", sim_folder, "Simulation folder");
+    loop->add_option("chart", chart_pdf, "Chart PDF");
 
-    using namespace std::chrono_literals;
-    auto poll_interval_ms = 500ms;
+    auto sim = app.add_subcommand("sim", "Simulate scenario and exit");
+    sim->add_option("simfile", scn_file, "Scenario file");
+    sim->add_option("simfolder", sim_folder, "Simulation folder");
 
-    groho::Simulator simulator(opt.scenario_file, opt.output_folder);
+    auto plot = app.add_subcommand("chart", "Chart simulation and exit");
+    plot->add_option("plotfile", plot_file, "Plot file");
+    plot->add_option("simfolder", sim_folder, "Simulation folder");
+    plot->add_option("chart", chart_pdf, "Chart PDF");
 
-    while (keep_running && simulator.ok()) {
-        if (simulator.scenario_changed()) {
-            simulator.restart();
-        }
-        std::this_thread::sleep_for(poll_interval_ms);
-    }
-
-    simulator.exit();
+    CLI11_PARSE(app, argc, argv);
 
     return 0;
 }
