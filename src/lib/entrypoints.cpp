bb2fa3f461c3541a1f3830b70de63d884a3ea587
Kaushik Ghose <kaushik.ghose@gmail.com>
2020-05-03 08:15:51 -0400

    Add simulator entrypoint stubs

---
+++ src/lib/entrypoints.cpp
@@ -0,0 +1,26 @@
+/*
+This file is part of Groho, a simulator for inter-planetary travel and warfare.
+Copyright (c) 2017-2018 by Kaushik Ghose. Some rights reserved, see LICENSE
+*/
+
+#include "entrypoints.hpp"
+
+namespace groho {
+
+void loop(
+    std::string scn_file,
+    std::string plot_file,
+    std::string sim_folder,
+    std::string chart_pdf)
+{
+    ;
+}
+
+void sim(std::string scn_file, std::string sim_folder) { ; }
+
+void chart(std::string plot_file, std::string sim_folder, std::string chart_pdf)
+{
+    ;
+}
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ Entry point function for command line program
 
 #include "CLI11.hpp"
 
+#include "entrypoints.hpp"
+
 void print_license()
 {
     std::cout << R"(  
@@ -35,15 +37,19 @@ int main(int argc, char* argv[])
     loop->add_option("plotfile", plot_file, "Plot file");
     loop->add_option("simfolder", sim_folder, "Simulation folder");
     loop->add_option("chart", chart_pdf, "Chart PDF");
+    loop->callback(
+        [&]() { groho::loop(scn_file, plot_file, sim_folder, chart_pdf); });
 
     auto sim = app.add_subcommand("sim", "Simulate scenario and exit");
     sim->add_option("simfile", scn_file, "Scenario file");
     sim->add_option("simfolder", sim_folder, "Simulation folder");
+    sim->callback([&]() { groho::sim(scn_file, sim_folder); });
 
-    auto plot = app.add_subcommand("chart", "Chart simulation and exit");
-    plot->add_option("plotfile", plot_file, "Plot file");
-    plot->add_option("simfolder", sim_folder, "Simulation folder");
-    plot->add_option("chart", chart_pdf, "Chart PDF");
+    auto chart = app.add_subcommand("chart", "Chart simulation and exit");
+    chart->add_option("plotfile", plot_file, "Plot file");
+    chart->add_option("simfolder", sim_folder, "Simulation folder");
+    chart->add_option("chart", chart_pdf, "Chart PDF");
+    chart->callback([&]() { groho::chart(plot_file, sim_folder, chart_pdf); });
 
     CLI11_PARSE(app, argc, argv);
 
